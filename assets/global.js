aa54ad04b6126258a7ee6981254c5cfdbe96da4e
bobby-216digital <95427013+bobby-216digital@users.noreply.github.com>
2026-08-27 14:13:11 -0400

    round one accessibility edits

--- assets/global.js
+++ assets/global.js
@@ -7,7 +7,6 @@ function getFocusableElements(container) {
 }
 
 document.querySelectorAll('[id^="Details-"] summary').forEach((summary) => {
-  summary.setAttribute('role', 'button');
   summary.setAttribute('aria-expanded', summary.parentNode.hasAttribute('open'));
 
   if (summary.nextElementSibling.getAttribute('id')) {
